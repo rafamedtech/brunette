518e678adf6d091a641cd8414f9811f8fc85804a
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2025-02-18 12:34:20 -0800

    Refactor EventModal.vue for improved readability and add usePageHead and useSurvey composables

--- components/EventModal.vue
+++ components/EventModal.vue
@@ -2,8 +2,7 @@
 const store = useEventStore();
 const { eventModal } = storeToRefs(store);
 
-const modalImage =
-  'https://res.cloudinary.com/rafamed-dev/image/upload/v1706122283/events/allyoucaneat_rbxihs.jpg';
+const modalImage = 'https://res.cloudinary.com/rafamed-dev/image/upload/v1706122283/events/allyoucaneat_rbxihs.jpg';
 
 function closeModal() {
   eventModal.value = false;
@@ -15,15 +14,10 @@ function closeModal() {
     class="fixed inset-0 z-[99999] grid min-h-screen w-screen place-content-center px-4 backdrop-brightness-[.2]"
   >
     <dialog class="bg-dark-strong h-9/10 card relative w-full max-w-lg rounded-xl shadow-xl">
-      <figure class="rounded-xl lg:w-80">
+      <figure class="w-80 rounded-xl">
         <img :src="modalImage" alt="" />
       </figure>
-      <button
-        class="btn btn-primary absolute -top-4 right-0 rounded-xl normal-case"
-        @click="closeModal"
-      >
-        Cerrar
-      </button>
+      <button class="btn btn-primary absolute -top-4 right-0 rounded-xl normal-case" @click="closeModal">Cerrar</button>
     </dialog>
   </section>
 </template>
