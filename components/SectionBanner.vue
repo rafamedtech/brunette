16c2271118b85e7e3a652528165aace12fdcee75
Rafael <rafamed.tech@gmail.com>
2025-02-28 15:30:58 -0800

    Remove unused menu background image and refactor SectionBanner component layout

--- components/SectionBanner.vue
+++ components/SectionBanner.vue
@@ -7,20 +7,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="relative flex h-32 w-full flex-col justify-center lg:h-40 lg:rounded-xl lg:shadow-xl">
-    <img
-      class="rounded-box absolute inset-0 h-full w-full object-cover brightness-[0.25]"
-      src="@/assets/images/menu-background.jpeg"
-      alt=""
-    />
-
-    <h2 class="z-10 text-center font-montserrat text-2xl text-base-100">
+  <div class="flex w-full flex-col items-center rounded-xl border-t py-4 lg:h-40 lg:rounded-xl lg:shadow-xl">
+    <h2 class="font-montserrat text-3xl font-semibold underline">
       {{ section.title }}
     </h2>
-    <p
-      class="text-center font-handlee text-lg font-extralight uppercase text-gray-50"
-      v-if="section.description"
-    >
+    <p class="text-center font-handlee text-lg font-extralight uppercase text-gray-50" v-if="section.description">
       {{ section.description }}
     </p>
   </div>
