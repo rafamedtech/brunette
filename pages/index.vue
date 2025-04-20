6de8d79b9353f8fc178f77f2449ae0f75373d641
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2025-04-20 11:30:26 -0700

    refactor: replace useHead with useSeoMeta for improved SEO metadata handling

--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,11 @@ const { language } = storeToRefs(store);
 
 const { menu } = useMenu();
 
-useHead({
+useSeoMeta({
   title: 'Brunette Kitchen & Drinks | Menú 📖',
-  meta: [
-    {
-      name: 'description',
-      content: 'Conoce nuestros deliciosos platillos y mixología.',
-    },
-    // Facebook Meta tags
-    {
-      property: 'og:type',
-      content: 'website',
-    },
-    {
-      property: 'og:url',
-      content: 'https://brunette.com.mx/',
-    },
-    {
-      property: 'og:title',
-      content: 'Brunette Kitchen & Drinks | Menú 📖',
-    },
-    {
-      property: 'og:description',
-      content: 'Conoce nuestros deliciosos platillos y mixología.',
-    },
-    {
-      property: 'og:image',
-      content: 'https://res.cloudinary.com/rafamed-dev/image/upload/v1705703429/menu/OG_Image_cl4k6w.png',
-    },
-  ],
+  description: 'Conoce nuestros deliciosos platillos y mixología.',
+  ogTitle: 'Brunette Kitchen & Drinks | Menú 📖',
+  ogImage: 'https://res.cloudinary.com/rafamed-dev/image/upload/v1705703429/menu/OG_Image_cl4k6w.png',
 });
 </script>
 
